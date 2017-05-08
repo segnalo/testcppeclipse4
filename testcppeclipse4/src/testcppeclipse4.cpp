42ed2d0d80ce2f256a8aa5db5d12c514d0cb4e46
Segna Lo <segnalo@users.noreply.github.com>
2017-05-08 19:07:57 -0400

    Added hello 1 and 2

--- testcppeclipse4/src/testcppeclipse4.cpp
+++ testcppeclipse4/src/testcppeclipse4.cpp
@@ -10,6 +10,7 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World1!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World2!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
