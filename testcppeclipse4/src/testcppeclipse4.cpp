7830cb0a8858fa1f9b201474e15244e9373e1cb0
Segna Lo <segnalo@users.noreply.github.com>
2017-05-08 18:38:31 -0400

    First commit with hello

---
+++ testcppeclipse4/src/testcppeclipse4.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : testcppeclipse4.cpp
+// Author      : Lo
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
